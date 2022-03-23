eb6e8e01a849da7fa923a55ee4f4e5bf4d79e4a7
Alaa Mohammed A Alhelal <alalhelal@elm.sa>
2022-03-23 09:39:49 +0300

    add image content description move the api key add version extension to library

--- app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -1,7 +1,9 @@
 package com.udacity.asteroidradar
 
+
+
 object Constants {
-    const val API_KEY = "tldfCaHy5yjrXACp2yedmxCp7OEkQ7hBpMAhkan8"
+    const val API_KEY = BuildConfig.NASA_API_KEY
     const val API_QUERY_DATE_FORMAT = "yyyy-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov"
